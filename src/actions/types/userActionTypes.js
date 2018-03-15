6564c6cdd08d2cd18a310f730157f5822cf680e4
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-03-16 00:09:47 +0800

    feat: add user action types

---
+++ src/actions/types/userActionTypes.js
@@ -0,0 +1,11 @@
+export const LOGIN = 'LOGIN'
+
+export const LOGIN_FAILED = 'LOGIN_FAILED'
+
+export const REGISTER_USER = 'REGISTER_USER'
+
+export const REGISTER_USER_FAILED = 'REGISTER_USER_FAILED'
+
+export const USER_LOGGING_IN = 'USER_LOGGING_IN'
+
+export const LOGOUT = 'LOGOUT'
