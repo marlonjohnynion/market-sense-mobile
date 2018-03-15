d2727ad74a5dcf05da45c2b9ef43a26bfa18b098
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-03-16 00:10:35 +0800

    feat: add basic user actions

---
+++ src/actions/userActions.js
@@ -0,0 +1,47 @@
+import * as actions from './types/userActionTypes'
+import firebase from '../common/firebase'
+
+export const loginSuccess = (values) => ({
+  type: actions.LOGIN,
+  email: values.email,
+  password: values.password
+})
+
+export const loginFail = (error) => ({
+  type: actions.LOGIN_FAILED,
+  error: error
+})
+
+export const registerFail = (error) => ({
+  type: actions.REGISTER_USER_FAILED,
+  error: error
+})
+
+export const registerUser = (email, password) => {
+  return async (dispatch) => {
+    try {
+      await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+      dispatch(loginSuccess(email, password))
+    } catch (e) {
+      dispatch(registerFail(e))
+    }
+  }
+}
+
+export const authenticateUser = (values) => {
+  return async (dispatch) => {
+    const {email, password} = values
+    try {
+      await firebase
+        .auth()
+        .signInAndRetrieveDataWithEmailAndPassword(email, password)
+      if (firebase.auth().currentUser) {
+        dispatch(loginSuccess(values))
+      }
+    } catch (e) {
+      dispatch(loginFail(e))
+    }
+  }
+}
