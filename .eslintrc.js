6515c784665c84b58ff0ee62998b4ffc9e2ef94f
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-03-15 23:32:28 +0800

    chore: configure linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-    "extends": "standard"
-};
+  "extends": "standard",
+  "plugins": [
+    "react",
+    "react-native"
+  ],
+  "ecmaFeatures": {
+    "jsx": true
+  },
+  "parser": "babel-eslint",
+  "env": {
+    "react-native/react-native": true
+  },
+  "rules": {
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react/jsx-uses-vars": 2
+  },
+  "globals": {
+    "it": 0,
+    "expect": 0,
+    "describe": 0,
+    "navigator": 0
+  }
+}
