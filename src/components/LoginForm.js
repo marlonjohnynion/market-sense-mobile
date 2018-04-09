d9cf126e14f977181bdaadc8a3d3d0a8b5d89153
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-09 15:12:22 +0800

    refactor: add header image

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Image } from 'react-native'
 import { Form, Item, Input, Label, Button, Text, Container } from 'native-base'
 
 export const emailField = (props) => {
@@ -16,19 +17,22 @@ export const LoginForm = (props) => {
   const { handleSubmit, loginHandler } = props
   return (
     <Container>
-      <Form>
-        <Item inlineLabel>
-          <Label>Email</Label>
-          <Field name='email' component={emailField}/>
-        </Item>
-        <Item inlineLabel last>
-          <Label>Password</Label>
-          <Field name='password' component={passwordField}/>
-        </Item>
-      </Form>
-      <Button block light onPress={handleSubmit(loginHandler)}>
-        <Text>LOGIN</Text>
-      </Button>
+      <Image source={require('../assets/images/market.jpeg')} style={{height: 200, width: null}} resizeMethod={'scale'}/>
+      <Container style={{marginTop: 20}}>
+        <Form>
+          <Item inlineLabel>
+            <Label>Email</Label>
+            <Field name='email' component={emailField}/>
+          </Item>
+          <Item inlineLabel last>
+            <Label>Password</Label>
+            <Field name='password' component={passwordField}/>
+          </Item>
+        </Form>
+        <Button block light onPress={handleSubmit(loginHandler)} style={{marginTop: 20}}>
+          <Text>LOGIN</Text>
+        </Button>
+      </Container>
     </Container>
 
   )
