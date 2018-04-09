41b7a788ae75ad927c60de78b5f81ff1432a5800
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-10 00:00:21 +0800

    feat: add necessary component helpers

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -18,3 +18,23 @@ export function getCurrentRoute (navigationState) {
   }
   return route
 }
+
+export function generateInvoiceNumber() {
+  return String(new Date().getTime())
+}
+
+export function roundToTwoFixedDecimalPlaces(number) {
+  return parseFloat(number).toFixed(2)
+}
+
+export function getFullAddress (...args) {
+  let address = ''
+  args.forEach(arg => {
+    address += arg + ' '
+  })
+  return address
+}
+
+export function getDateInWords (date) {
+  return new Date(date).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
+}
