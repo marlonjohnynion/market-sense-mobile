742e8af2ad2616fe156caf3469dcc988aaf3011b
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-09 12:06:35 +0800

    build: add react natvigation current route dependency

---
+++ src/containers/NewOrder.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import {
+  Container,
+  Content
+} from 'native-base'
+import { connect } from 'react-redux'
+import { formValueSelector } from 'redux-form'
+import OrderForm from '../components/OrderForm'
+import { addOrder } from '../actions/orderActions'
+
+export const Order = ({ state, orderData, actions, selectedProduct }) => {
+  return (
+    <Container>
+      <Content>
+        <OrderForm orderData={orderData} submissionHandler={actions.addOrder} product={selectedProduct}/>
+      </Content>
+    </Container>
+  )
+}
+
+const selector = formValueSelector('orderForm')
+
+const mapStateToProps = (state) => ({
+  orderData: {
+    deliveryDate: selector(state, 'deliveryDate'),
+    quantity: selector(state, 'quantity')
+  },
+  selectedProduct: state.products.selectedProduct
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({ addOrder }, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Order)
