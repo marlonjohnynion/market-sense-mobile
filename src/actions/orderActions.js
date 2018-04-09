216e696fe5c345f499101440e1760b853535600d
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-10 00:06:40 +0800

    feat: add order actions used by Orders components

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,13 +1,58 @@
 import firebase from '../common/firebase'
+import { generateInvoiceNumber } from '../common/helpers'
 
 export const addOrder = (order) => {
   return async (dispatch, getState) => {
     const { selectedProduct } = getState().products
-    const newOrder = {
-      productKey: selectedProduct.key,
-      ...order
+    const parsedProductData = {
+      key: selectedProduct.key,
+      productPrice: parseFloat(selectedProduct.productPrice),
+      lotSize: parseFloat(selectedProduct.lotSize)
     }
-    const orderReference = await firebase.database().ref('/orders').push(newOrder)
-    dispatch({type: 'ADD_ORDER', order: newOrder})
+    const payload = {
+      ...order,
+      invoiceNumber: generateInvoiceNumber(),
+      quantity: parseInt(order.quantity),
+      product: {
+        ...parsedProductData
+      }
+    }
+    const newOrder = {
+      ...payload,
+      product: {
+        ...selectedProduct
+      }
+    }
+    try {
+      await firebase.database().ref('/orders').push(payload)
+      dispatch({ type: 'ADD_ORDER', order: newOrder })
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+}
+
+export const loadOrders = () => {
+  return async (dispatch) => {
+    try {
+      const orders = []
+      const data = await firebase.database().ref('/orders').once('value')
+      for (const [key, resData] of Object.entries(data.val())) {
+        const order = {
+          key: key,
+          ...resData
+        }
+        const { product } = order
+        const productData = await firebase.database().ref(`/products/${product.key}`).once('value')
+        order.product = {
+          ...productData.val(),
+          ...order.product
+        }
+        orders.push(order)
+      }
+      dispatch({ type: 'LOAD_ORDERS', orders: orders })
+    } catch (e) {
+      throw new Error(e)
+    }
   }
 }
