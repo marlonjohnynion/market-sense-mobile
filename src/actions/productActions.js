d9ec421e446d469908b2bf6f4f3fdd5ef9579987
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-10 00:04:27 +0800

    refactor: seprate dispatch for loading and showing products

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -10,37 +10,21 @@ export const loadProducts = () => {
         ...child.val()
       })
     })
-    dispatch(viewProducts(products))
-  }
-}
-
-export const addProduct = () => {
-  return async (dispatch) => {
-    const addProducts = await firebase.database().ref('/products/1').set({
-      productTitle: 'Carrots',
-      productPrice: '₱1500 / 10 kg',
-      productLocation: 'OTON, ILOILO',
-      productDescription: 'Perfect accompaniment for mains and sandwiches. Ideal edible bed for roasted...',
-      productImageUri: 'https://firebasestorage.googleapis.com/v0/b/marketsense-b09f8.appspot.com/o/products%2Fp3.png?alt=media&token=7fd95509-ea2b-4188-b85e-218609d5ee26'
+    dispatch({
+      type: 'LOAD_PRODUCTS',
+      products: products
     })
-    console.log(addProducts)
+    dispatch({
+      type: 'SHOW_PRODUCTS'
+    })
   }
 }
 
-export const checkProductAvailability = (product) => {
+export const checkProductAvailability = () => {
   return async (dispatch) => {
-    dispatch(viewProductAvailability())
+    dispatch({type: 'VIEW_PRODUCT_AVAILABILITY'})
   }
 }
-
-export const viewProductAvailability = () => ({
-  type: 'VIEW_PRODUCT_AVAILABILITY'
-})
-
-export const viewProducts = (products) => ({
-  type: 'SHOW_PRODUCTS',
-  products: products
-})
 
 export const selectProduct = (product) => ({
   type: 'SELECT_PRODUCT',
