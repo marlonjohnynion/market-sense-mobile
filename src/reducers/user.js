57371c0dc473631efa2c671ef5fa0f67ec521d63
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-11 06:16:44 +0800

    refactor: include user id to application state

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -3,7 +3,7 @@ import * as actions from '../actions/types/userActionTypes'
 const user = (state = { loggedIn: false }, action) => {
   switch (action.type) {
     case actions.LOGIN:
-      return { ...state, loggedIn: true, email: action.email }
+      return { ...state, loggedIn: true, email: action.email, uid: action.uid }
     case actions.LOGOUT:
       return { ...state, loggedIn: false }
     default:
