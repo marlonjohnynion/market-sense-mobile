1f3863bfcc3ff10c5345805b6a755fbac358b85f
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-11 14:37:41 +0800

    refactor: make hiddenRow as a variable component

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -4,15 +4,20 @@ import { ListView } from 'react-native'
 import OrdersListItem from './OrdersListItem'
 
 export const DataList = ({ data, actions, RowComponent, RightHiddenRowComponent }) => {
+  const HiddenComponent = (data, actions) => {
+    if (RightHiddenRowComponent) {
+      return <RightHiddenRowComponent data={data} actions={actions}/>
+    }
+    return undefined
+  }
   const ds = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 })
-  console.log(actions)
   return (
     <Container>
       <Content>
         <List
           dataSource={ds.cloneWithRows(data)}
           renderRow={rowData => <RowComponent data={rowData} actions={actions}/>}
-          renderRightHiddenRow={rowData => <RightHiddenRowComponent data={rowData} actions={actions}/>}
+          renderRightHiddenRow={HiddenComponent}
           rightOpenValue={-75}>
         </List>
       </Content>
