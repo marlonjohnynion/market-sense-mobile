5741e65d4e9367c4f092999c5f7c69934f52dded
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-12 07:43:13 +0800

    refactor: change into sfc

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux'
 import store from './src/common/store'
 import MainContainer from './src/containers/MainContainer'
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <Provider store={store}>
-        <MainContainer />
-      </Provider>
-    )
-  }
+const App = () => {
+  return (
+    <Provider store={store}>
+      <MainContainer/>
+    </Provider>
+  )
 }
+
+export default App
