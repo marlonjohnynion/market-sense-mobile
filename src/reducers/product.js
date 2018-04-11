23a2867190b1b2a70c49648ca9041e156ac5fe87
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-11 12:17:32 +0800

    feat: add product update and delete reducers

--- src/reducers/product.js
+++ src/reducers/product.js
@@ -1,14 +1,26 @@
+import { insertItemToArray } from '../common/helpers'
+
 const product = (state = { productsList: [] }, action) => {
+  let products
   switch (action.type) {
     case 'LOAD_PRODUCTS':
       return { ...state, productsList: action.products.concat(state.productsList) }
     case 'SELECT_PRODUCT':
       return { ...state, selectedProduct: action.selectedProduct }
+    case 'EDIT_PRODUCT':
+      return { ...state, editedProduct: action.product }
     case 'ADD_PRODUCT':
-      const product = action.product
       const newProducts = [...state.products]
-      newProducts.push(product)
+      newProducts.push(action.product)
       return {...state, products: newProducts}
+    case 'UPDATE_PRODUCT':
+      let products = [...state.productsList]
+      products = products.filter(item => item.key !== action.product.key)
+      products.push(action.product)
+      return {...state, productsList: products, editedProduct: action.product}
+    case 'DELETE_PRODUCT':
+      const filteredProducts = [...state.productsList].filter(product => product.key !== action.product.key)
+      return {...state, productsList: filteredProducts}
     default:
       return state
   }
