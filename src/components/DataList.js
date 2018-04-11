294946fc5885de1bb9a9e199df4df77871f9d7f6
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-04-11 12:27:14 +0800

    refactor: make hidden right component as variable

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -3,14 +3,7 @@ import { Container, List, Content, Text, Icon, Button } from 'native-base'
 import { ListView } from 'react-native'
 import OrdersListItem from './OrdersListItem'
 
-const Orders = ({ orders }) => {
-  if (orders) {
-    return orders.map((order) => <OrdersListItem key={order.invoiceNumber} order={order}/>)
-  }
-  return <Text>No Orders Found!</Text>
-}
-
-export const DataList = ({ data, actions, RowComponent }) => {
+export const DataList = ({ data, actions, RowComponent, RightHiddenRowComponent }) => {
   const ds = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 })
   console.log(actions)
   return (
@@ -19,7 +12,7 @@ export const DataList = ({ data, actions, RowComponent }) => {
         <List
           dataSource={ds.cloneWithRows(data)}
           renderRow={rowData => <RowComponent data={rowData} actions={actions}/>}
-          renderRightHiddenRow={order => <Button full onPress={() => console.log(order)}><Icon active name={'trash'} /></Button>}
+          renderRightHiddenRow={rowData => <RightHiddenRowComponent data={rowData} actions={actions}/>}
           rightOpenValue={-75}>
         </List>
       </Content>
